f54d6eddcb5ee71d07e1fc9e946a45ad61809d41
Sevenc1over <thomas.h.jeong@gmail.com>
2025-12-12 00:12:35 +0900

    file dbconnect.py

--- jung/dbconnect.py
+++ jung/dbconnect.py
@@ -4,7 +4,7 @@ import pandas as pd
 conn = pymysql.connect(
     host="localhost",        # 내부 실행이므로 OK
     user="dbid253",
-    password="여기에_실제비번",
+    password="dbpass253",
     db="db25320",
     charset="utf8mb4"
 )
