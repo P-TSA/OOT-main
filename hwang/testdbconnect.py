0e0e72c0093652242c8d41cc5a0af5d5c38da61a
Sevenc1over <thomas.h.jeong@gmail.com>
2025-12-11 23:39:47 +0900

    file testdbconnect.py

---
+++ hwang/testdbconnect.py
@@ -0,0 +1,9 @@
+import pymysql
+
+conn = pymysql.connect(
+            host="localhost",
+            user="dbid253",
+            password="dbpass253",
+            db="db25320",
+            charset="utf8mb4"
+)
